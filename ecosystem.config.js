12a0695cc35d9be7dee3328193f967f1fb3249bf
pll2050 <pll2050@gmail.com>
2023-01-27 15:42:55 +0000

    edited ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+      name: 'pm2-test',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
   }],
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      user : 'ksk',
+      key  : '/home/ksk/web/key/pll2050@vmware.pub',
+      host : '192.168.0.119',
+      ref  : 'origin/main',
+      repo : 'git@github.com:pll2050/pm2-test.git',
+      path : '/home/ksk/web/pm2-production',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && pm2 start ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
